bcc4dec1f39994b66701655149817cc892ccc68d
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-10-19 23:36:09 -0400

    linked list

--- contribution/masaka222/hacktoberfest.java
+++ contribution/masaka222/hacktoberfest.java
@@ -1,3 +1,54 @@
+public class Node {
+    Node next;
+    int data;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+public class LinkedList {
+    Node head;
+
+    public void append(int data){
+        if(head == null){
+            head = new Node(data);
+            return;
+        }
+        Node current = head;
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = new Node(data);
+    }
+
+    public void prepend(int data){
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
+    }
+
+    public void deleteWithValue(int data){
+        if (head == null){
+            return;
+        }
+        if (head.data == data){
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        while(current.next != null){
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+}
+
+
 public class hacktoberfest {
     public static void main(String[] args) {
         System.out.println("Hacktoberfest initial program");
