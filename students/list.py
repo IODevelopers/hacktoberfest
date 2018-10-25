9cadc4ed5f066994e1467c91c88c00e6027f1f14
sachinsaju <33017477+sachinsaju@users.noreply.github.com>
2018-10-25 15:03:32 +0530

    list.py

---
+++ students/list.py
@@ -0,0 +1,11 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
