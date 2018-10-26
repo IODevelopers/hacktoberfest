7959689a6519a80ed6e9e3925b8e18141b553908
Wisnu Febriadi <idtfc.febriady@gmail.com>
2018-10-27 01:47:01 +0700

    Create java_helloworld_EN_ID_RU.java

---
+++ students/java_helloworld_EN_ID_RU.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Привет, мир");
+        System.out.println("Halo, Dunia");
+    }
+
+}
