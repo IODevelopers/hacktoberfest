2ca2c60533c9bb10afcdb50c7c1a04db2335f234
thabitha333 <42231686+thabitha333@users.noreply.github.com>
2018-10-27 23:37:39 +0530

    Controlling a dc motors using python with raspberryPi
    the motor can be turned both forward and backwards, the sleep time provided is 2 seconds.

---
+++ thabitha/t2.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+GPIO.setmode(GPIO.BOARD)
+ 
+Motor1A = 16
+Motor1B = 18
+Motor1E = 22
+ 
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor1E,GPIO.OUT)
+ 
+print "Going forwards"
+GPIO.output(Motor1A,GPIO.HIGH)
+GPIO.output(Motor1B,GPIO.LOW)
+GPIO.output(Motor1E,GPIO.HIGH)
+ 
+sleep(2)
+ 
+print "Going backwards"
+GPIO.output(Motor1A,GPIO.LOW)
+GPIO.output(Motor1B,GPIO.HIGH)
+GPIO.output(Motor1E,GPIO.HIGH)
+ 
+sleep(2)
+ 
+print "Now stop"
+GPIO.output(Motor1E,GPIO.LOW)
+ 
+GPIO.cleanup()
