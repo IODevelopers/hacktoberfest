ba6d96d43c82d19e249d4c7c931e4b069814f115
Nevac14 <43809134+Nevac14@users.noreply.github.com>
2018-10-31 19:51:03 +0530

    Create Nevin5.py

---
+++ students/Nevin5.py
@@ -0,0 +1,8 @@
+n=int(input("Enter a number"))
+for i in range(n):
+      for j in range(n):
+            print(j*i,end=" ")
+      print()
+      
+#the given pgrm displays a simple pattern
+            
